9828bad4553ff1521c1d72d881dc07c52acd58a7
harsh-gupta02 <harsh.gupta@affordplan.com>
2025-10-11 22:56:58 +0530

    ai config changes

--- lib/openrouter.ts
+++ lib/openrouter.ts
@@ -9,8 +9,9 @@ export async function openRouter(messages: CoreMessage[]) {
   const result = streamText({
     model: openrouter.chat('openai/gpt-5-chat'),
     messages,
-    maxOutputTokens: 100
-  });
+    maxOutputTokens: 2000,
+    temperature: 0.7
+  })
 
   return result;
 }
